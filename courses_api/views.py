ed3807976ec42a971d066449429d189c27b79744
Abdul-Muqadim-Arbisoft <139064778+Abdul-Muqadim-Arbisoft@users.noreply.github.com>
2023-10-23 11:03:07 +0500

    views added for the courses list api

--- courses_api/views.py
+++ courses_api/views.py
@@ -1,11 +1,15 @@
 """
 views for edx_course.
 """
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Courses
+from rest_framework.generics import ListAPIView
+from .serializers import CoursesSerializer
 
 
-def hello_template(request):
-    """
-    Test working functionality.
-    """
-    return render(request, 'courses_api/hello.html')
+@api_view(['GET'])
+def get_courses(request):
+    courses = Courses.objects.all()
+    serializer = CoursesSerializer(courses, many=True)
+    return Response({'courses': serializer.data})
