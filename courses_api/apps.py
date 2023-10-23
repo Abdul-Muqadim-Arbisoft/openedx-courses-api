114c4cbaaade64d58160e4cf3f168fe0207040d0
Abdul-Muqadim-Arbisoft <abdul.muqadim@arbisoft.com>
2023-10-23 14:26:22 +0500

    changes made to the apps configuration removed

--- courses_api/apps.py
+++ courses_api/apps.py
@@ -1,25 +1,25 @@
-"""
-courses_api Django application initialization.
-"""
+# """
+# courses_api Django application initialization.
+# """
 from django.apps import AppConfig
-from edx_django_utils.plugins.constants import (
-    PluginURLs
-)
-from openedx.core.djangoapps.plugins.constants import ProjectType
-
-
+# from edx_django_utils.plugins.constants import (
+#     PluginURLs
+# )
+# from openedx.core.djangoapps.plugins.constants import ProjectType
+#
+#
 class CoursesApiConfig(AppConfig):
     """
     Configuration for the courses_api Django application.
     """
-
+#
     name = 'courses_api'
-    plugin_app = {
-        PluginURLs.CONFIG: {
-            ProjectType.LMS: {
-                PluginURLs.NAMESPACE: 'courses_api',
-                PluginURLs.REGEX: r'^api/courses_api/',
-                PluginURLs.RELATIVE_PATH: 'urls',
-            }
-        }
-    }
+#     plugin_app = {
+#         PluginURLs.CONFIG: {
+#             ProjectType.LMS: {xx
+#                 PluginURLs.NAMESPACE: 'courses_api',
+#                 PluginURLs.REGEX: r'^api/courses_api/',
+#                 PluginURLs.RELATIVE_PATH: 'urls',
+#             }
+#         }
+#     }
