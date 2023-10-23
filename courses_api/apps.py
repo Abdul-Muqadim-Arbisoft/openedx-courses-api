a400d816a4060341e39dd7399a6750005bea8486
Abdul-Muqadim-Arbisoft <abdul.muqadim@arbisoft.com>
2023-10-23 15:15:30 +0500

    commendted out code removed

--- courses_api/apps.py
+++ courses_api/apps.py
@@ -2,10 +2,10 @@
 courses_api Django application initialization.
 """
 from django.apps import AppConfig
-# from edx_django_utils.plugins.constants import (
-#     PluginURLs
-# )
-# from openedx.core.djangoapps.plugins.constants import ProjectType
+from edx_django_utils.plugins.constants import (
+    PluginURLs
+)
+from openedx.core.djangoapps.plugins.constants import ProjectType
 
 
 class CoursesApiConfig(AppConfig):
@@ -14,13 +14,13 @@ class CoursesApiConfig(AppConfig):
     """
 
     name = 'courses_api'
-    # plugin_app = {
-    #     PluginURLs.CONFIG: {
-    #         ProjectType.LMS: {
-    #             PluginURLs.NAMESPACE: 'courses_api',
-    #             PluginURLs.APP_NAME: 'courses_api',
-    #             PluginURLs.REGEX: r'^api/courses_api/',
-    #             PluginURLs.RELATIVE_PATH: 'urls',
-    #         }
-    #     }
-    # }
+    plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: 'courses_api',
+                PluginURLs.APP_NAME: 'courses_api',
+                PluginURLs.REGEX: r'^api/courses_api/',
+                PluginURLs.RELATIVE_PATH: 'urls',
+            }
+        }
+    }
--- courses_api/urls.py
+++ courses_api/urls.py
@@ -5,7 +5,6 @@ from django.urls import path
 
 from courses_api.views import CourseListAPIView
 
-
 urlpatterns = [
     path('list/', CourseListAPIView.as_view(), name='course_list'),
 ]
