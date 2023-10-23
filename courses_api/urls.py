26f3c22af378932402bc425775cc41b5247613d3
Abdul-Muqadim-Arbisoft <139064778+Abdul-Muqadim-Arbisoft@users.noreply.github.com>
2023-10-23 11:02:41 +0500

    api url created for the cources list

--- courses_api/urls.py
+++ courses_api/urls.py
@@ -1,11 +1,8 @@
-"""
-URLs for courses_api.
-"""
 from django.urls import path
-from .views import hello_template
+from courses_api.views import get_courses
 
 urlpatterns = [
-    # TODO: Fill in URL patterns and views here.
-    re_path(r'/abc', TemplateView.as_view(template_name="courses_api/base.html")),
-    #path('hello_template/', hello_template, name='hello_template'),
+    path(
+        'list/', get_courses, name='get_courses_list'
+    )
 ]
